05ca4d449d10a80074250e293e7fdf48e246e61f
cldurbin <calebldurbin@yahoo.com>
2025-07-11 01:10:19 +0900

    Initial commit with working Clerk auth

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+// src/middleware.ts
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/sign-in", "/sign-up"],
+});
+
+export const config = {
+  matcher: ["/((?!_next|.*\\..*).*)"],
+};
